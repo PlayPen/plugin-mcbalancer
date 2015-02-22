23f0f49a1822f0d24c1afe3566c4d7347ced43a7
Sam Bloomberg <sam@redxdev.com>
2015-02-22 13:39:02 -0500

    initial creation of project

---
+++ src/main/java/net/thechunk/playpen/plugin/mcbalancer/MCBalancerPlugin.java
@@ -0,0 +1,14 @@
+package net.thechunk.playpen.plugin.mcbalancer;
+
+import net.thechunk.playpen.plugin.AbstractPlugin;
+
+public class MCBalancerPlugin extends AbstractPlugin {
+    @Override
+    public boolean onStart() {
+        return true;
+    }
+
+    @Override
+    public void onStop() {
+    }
+}
